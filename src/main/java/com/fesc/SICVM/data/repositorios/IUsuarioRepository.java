30271dd1c42e61250f7023e1b61bedc2a39ec97f
Valen1509 <117381859+Valen1509@users.noreply.github.com>
2023-05-19 22:46:40 -0500

    Delete src directory

--- src/main/java/com/fesc/SICVM/data/repositorios/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fesc.SICVM.data.repositorios;
-
-import com.fesc.SICVM.data.entidades.UsuarioEntity;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface IUsuarioRepository extends CrudRepository<UsuarioEntity, Long> {
-
-}
--- src/main/java/com/fesc/SICVM/services/IUsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.fesc.SICVM.services;
-
-import com.fesc.SICVM.shared.UsuarioDto;
-
-public interface IUsuarioService {
-
-    UsuarioDto crearUsuario(UsuarioDto usuarioCrearDto);
-
-}
